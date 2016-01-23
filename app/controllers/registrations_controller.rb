a561666d7de48c2d1f45b85b93ab6d6ae8375772
JosephWinfield <krazyjoe18@gmail.com>
2016-01-22 19:59:54 -0500

    changes default for sign-in and sign-up paths

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,12 @@
+class RegistrationsController < Devise::RegistrationsController
+
+def show
+  super
+end
+
+protected
+
+  def after_update_path_for(current_user)
+    user_path(current_user)
+  end
+end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,4 @@
 class Place < ActiveRecord::Base
+  belongs_to :user
+  
 end
