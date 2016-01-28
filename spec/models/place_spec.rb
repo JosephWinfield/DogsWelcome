b377c06351141c1fe5a86a6176f6240bfc899df8
JosephWinfield <krazyjoe18@gmail.com>
2016-01-27 21:47:28 -0500

    adds model tests for Places

---
+++ spec/models/place_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+describe Place do
+  it "should have a name attribute" do
+    should have_valid(:name).when('Foo')
+  end
+  it "should not have a blank name attribute" do
+    should_not have_valid(:name).when(nil, '')
+  end
+
+  it "should have an address attribute" do
+    should have_valid(:address).when('1234 Harrison Ave')
+  end
+  it "should not have a blank address attribute" do
+    should_not have_valid(:address).when(nil, '')
+  end
+
+  it "should have a valid city" do
+    should have_valid(:city).when('Boston')
+  end
+
+  it "should not have a valid city" do
+    should_not have_valid(:city).when(nil, "",)
+  end
+
+  it "should have a valid state" do
+    should have_valid(:state).when('MA')
+  end
+
+  it "should not have a valid state" do
+    should_not have_valid(:state).when(nil, "",)
+  end
+
+  it "should have a valid zip" do
+    should have_valid(:zip).when('01234')
+  end
+
+  it "should not have a valid zip" do
+    should_not have_valid(:zip).when(nil, "",)
+  end
+
+  it "should have a valid category" do
+    should have_valid(:category).when('Park')
+  end
+
+  it "should not have a valid category" do
+    should_not have_valid(:category).when(nil, "",)
+  end
+end
